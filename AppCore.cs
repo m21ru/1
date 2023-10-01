acaca2d8b7c3532bc5668a609950d7a662b6a3fc
m.a.shcheglov@ya.ru <USdzDWI8wLoow6rL>
2023-10-01 18:06:18 +0300

    add core in app

---
+++ AppCore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace cmd1
+{
+    public class AppCore
+    {
+        public AppCore()
+        {
+        }
+
+        /// <summary>
+        /// Add new html function
+        /// </summary>
+        public void AddNewHtml(string html)
+        {
+
+        }
+    }
+}
